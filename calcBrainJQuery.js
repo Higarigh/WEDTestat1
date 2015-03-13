e4fd74c355415d7c20745eaa065c624d76ade939
Philip Schmid <philip.schmid@outlook.com>
2015-03-13 09:31:14 +0100

    jquery conflict handling added

--- calcBrainJQuery.js
+++ calcBrainJQuery.js
@@ -1,54 +1,57 @@
 /**
  * Created by phil on 06.03.15.
  */
+jQuery.noConflict();
+(function( $ ) {
 
-$(function() {
-    console.log( "JQuery loaded!" );
+    $(function() {
+        console.log( "JQuery loaded!" );
         $("body").on("click", "input[type=button]", function() {
             onClickCalculations(this.name);
         });
-});
+    });
 
-var calculated;
-var pattern = new RegExp("[^0-9]");
+    var calculated;
+    var pattern = new RegExp("[^0-9]");
 
-function onClickCalculations(name){
-    displayValue = $('#output').val();
+    function onClickCalculations(name){
+        displayValue = $('#output').val();
 
-    switch (name){
-        case "=":
-            displayValue = eval(displayValue);
-            calculated = true;
-            break;
-        case "+":
-        case "-":
-        case "*":
-        case "/":
-            if(pattern.test(displayValue.slice(-1))) {
-                displayValue = displayValue.slice(0,displayValue.length - 2) + name + " ";
-                console.log("NOT A NUMBER")
-            }else{
-                displayValue += " " + name + " ";
-            }
-            calculated = false;
-
-
-            break;
-        default:
-            if(calculated){
-                displayValue = 0;
-
+        switch (name){
+            case "=":
+                displayValue = eval(displayValue);
+                calculated = true;
+                break;
+            case "+":
+            case "-":
+            case "*":
+            case "/":
+                if(pattern.test(displayValue.slice(-1))) {
+                    displayValue = displayValue.slice(0,displayValue.length - 2) + name + " ";
+                    console.log("NOT A NUMBER")
+                }else{
+                    displayValue += " " + name + " ";
+                }
                 calculated = false;
-            }
-            if(displayValue == 0){
-                displayValue = name;
-            }else {
 
-                displayValue += name;
-            }
 
-            break;
+                break;
+            default:
+                if(calculated){
+                    displayValue = 0;
+
+                    calculated = false;
+                }
+                if(displayValue == 0){
+                    displayValue = name;
+                }else {
+
+                    displayValue += name;
+                }
+
+                break;
+        }
+        $('#output').val(displayValue);
+
     }
-    $('#output').val(displayValue);
-    
-}
+})( jQuery );
