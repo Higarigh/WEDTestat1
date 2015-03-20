0320a54d0e12acc41662b5e6a7372fe2b294d386
Matthias Gabriel <matthias.gabriel@gmx.ch>
2015-03-20 09:27:13 +0100

    CodeStyle based on https://google-styleguide.googlecode.com/svn/trunk/javascriptguide.xml Used used for WED Testat 1 Part 5

--- calcBrainJQuery.js
+++ calcBrainJQuery.js
@@ -1,57 +1,59 @@
 /**
  * Created by phil on 06.03.15.
+ * This contains the functionality for the calculator used for the WEDTestat1
  */
+var WED_TESTAT = {};
+/**
+ * Regular Expression to evaluate the operators
+ * @const
+ */
+WED_TESTAT.REGEX_PATTERN = new RegExp("[^0-9]");
+/**
+ * Global boolean that checks if the expression was evaluated in the last step
+ */
+WED_TESTAT.CALCULATED = Boolean(false);
 jQuery.noConflict();
-(function( $ ) {
-
-    $(function() {
-        console.log( "JQuery loaded!" );
-        $("body").on("click", "input[type=button]", function() {
-            onClickCalculations(this.name);
-        });
-    });
-
-    var calculated;
-    var pattern = new RegExp("[^0-9]");
-
-    function onClickCalculations(name){
-        var displayValue = $('#output').val();
-
-        switch (name){
-            case "=":
-                displayValue = eval(displayValue);
-                calculated = true;
-                break;
-            case "+":
-            case "-":
-            case "*":
-            case "/":
-                if(pattern.test(displayValue.slice(-1))) {
-                    displayValue = displayValue.slice(0,displayValue.length - 2) + name + " ";
-                    console.log("NOT A NUMBER")
-                }else{
-                    displayValue += " " + name + " ";
-                }
-                calculated = false;
-
-
-                break;
-            default:
-                if(calculated){
-                    displayValue = 0;
-
-                    calculated = false;
-                }
-                if(displayValue === 0){
-                    displayValue = name;
-                }else {
-
-                    displayValue += name;
-                }
-
-                break;
-        }
-        $('#output').val(displayValue);
-
-    }
-})( jQuery );
+(function ($) {
+	$(function () {
+		$("body").on("click", "input[type=button]", function () {
+			onClickCalculations(this.name);
+		});
+	});
+	/**
+	 * Sets the onClickEvents based on the buttons name
+	 * @param {string} name This contains the buttons name
+	 */
+	function onClickCalculations(name) {
+		var display_value = $("#output").val();
+		switch (name) {
+			case "=":
+				display_value = eval(display_value);
+				WED_TESTAT.CALCULATED = Boolean(true);
+				break;
+			case "+":
+			case "-":
+			case "*":
+			case "/":
+				if (WED_TESTAT.REGEX_PATTERN.test(display_value.slice(-1))) {
+					display_value = display_value.slice(0,
+						display_value.length - 2) + name + " ";
+				} else {
+					display_value += " " + name + " ";
+				}
+				WED_TESTAT.CALCULATED = Boolean(false);
+				break;
+			default:
+				if (WED_TESTAT.CALCULATED) {
+					display_value = "0";
+					WED_TESTAT.CALCULATED = Boolean(false);
+				}
+				if(display_value==="0") {
+					display_value = name;
+				} else {
+					display_value += name;
+				}
+				break;
+		}
+		$("#output").val(display_value);
+	}
+})(jQuery);
